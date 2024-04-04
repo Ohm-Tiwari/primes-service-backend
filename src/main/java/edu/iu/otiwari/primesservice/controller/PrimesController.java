31088c3a77b1ed06ed66ffa5187068e3cc806409
Ohm-Tiwari <otiwari24@gmail.com>
2024-04-04 15:44:25 -0400

    changes

--- src/main/java/edu/iu/otiwari/primesservice/controller/PrimesController.java
+++ src/main/java/edu/iu/otiwari/primesservice/controller/PrimesController.java
@@ -1,5 +1,6 @@
 package edu.iu.otiwari.primesservice.controller;
 
+import edu.iu.otiwari.primesservice.rabbitmq.MQSender;
 import edu.iu.otiwari.primesservice.service.IPrimesService;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,13 +9,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/primes")
 public class PrimesController {
     IPrimesService primesService;
-    public PrimesController(IPrimesService primesService){
+
+    private final MQSender mqSender;
+    public PrimesController(IPrimesService primesService,
+                            MQSender mqSender){
         this.primesService = primesService;
+        this.mqSender = mqSender;
     }
 
 
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n){
-        return primesService.isPrime(n);
+        boolean result = primesService.isPrime(n);
+        mqSender.sendMessage(n, result);
+        return result;
     }
 }
--- src/main/java/edu/iu/otiwari/primesservice/repository/IAuthenticationRepository.java
+++ src/main/java/edu/iu/otiwari/primesservice/repository/IAuthenticationRepository.java
@@ -6,6 +6,5 @@ import java.io.IOException;
 
 public interface IAuthenticationRepository {
     boolean save(Customer customer) throws IOException;
-
     Customer findByUsername(String username) throws IOException;
 }
